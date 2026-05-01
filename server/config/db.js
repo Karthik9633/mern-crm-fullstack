7052ab221e4392213b916b40c11f8ac6d3a6e8ad
Karthik B <karthikbalu5533@gmail.com>
2026-05-01 23:52:41 +0530

    updated adn error was solveed

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error(err);
+
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ DB Error:", error.message);
     process.exit(1);
   }
 };
